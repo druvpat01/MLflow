b0347cb8ef07c7c754c9e683be20e8ef5769c4c7
Dhruv Patel <dhruv.patel@proclink>
2025-08-15 17:39:02 +0530

    updated main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import mlflow.sklearn
 import dagshub    # for storing the code in the cloud
 import logging
 
+# initalizing to Dagshub
+dagshub.init(repo_owner='druvpat', repo_name='MLflow', mlflow=True)
 
 # creating logger
 logging.basicConfig(level=logging.WARN)
@@ -23,8 +25,8 @@ logger = logging.getLogger(__name__)
 def eval(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
-    r2_score = r2_score(actual, pred)
-    return rmse, mae, r2_score
+    r2_value = r2_score(actual, pred)
+    return rmse, mae, r2_value
 
 if __name__ == '__main__':
     np.random.seed(40)
@@ -60,17 +62,31 @@ if __name__ == '__main__':
 
         pred = model.predict(test_x)
 
-        rmse, mae, r2_score = eval(test_y, pred)
+        rmse, mae, r2_value = eval(test_y, pred)
 
         print(f"ElasticNet model (alpha = {alpha}, l1_ratio = {l1_ratio})")
         print(' RMSE: ', rmse)
         print(' MAS: ',mae)
-        print(' R2_score: ', r2_score)
+        print(' R2_score: ', r2_value)
 
         mlflow.log_param('alpha', alpha)
         mlflow.log_param('l1_ratio', l1_ratio)
         mlflow.log_metric('rmse', rmse)
         mlflow.log_metric('mae', mae)
-        mlflow.log_metric('r2_score', r2_score)
+        mlflow.log_metric('r2_score', r2_value)
 
+        # for remote server (Dagashub)
+        remote_server_url = "https://dagshub.com/druvpat/MLflow.mlflow"
+        mlflow.set_tracking_uri(remote_server_url)
 
+        #parsed the URI into a structured object with components like schmem, netloc, path etc.
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        #Model 
+        if tracking_url_type_store != 'file':
+            #register url model
+            mlflow.sklearn.log_model(
+                model, "name", registered_model_name="ElasticNetWineModel"
+            )
+        else:
+            mlflow.sklearn.log_model(model,'model')
